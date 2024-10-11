53dd88387da95029006cad52946378371e096d34
hongsu.park <heavyflood@gmail.com>
2024-10-11 14:54:58 +0900

    init

--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -1,9 +1,7 @@
 package com.example.demo;
 
-import org.apache.maven.surefire.report.ReporterManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.apache.maven.surefire.report.Reporter;
 
 @RestController
 public class SampleController {
